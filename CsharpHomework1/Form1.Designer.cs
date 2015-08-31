789755ab75497dccb516de4a0be7e443355d3447
lovenery <edittye379@gmail.com>
2015-09-01 01:18:23 +0800

    init commit

---
+++ CsharpHomework1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CsharpHomework1
+{
+    partial class Homework1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Restart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberselector = new System.Windows.Forms.NumericUpDown();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.HSlabel = new System.Windows.Forms.Label();
+            this.Slabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numberselector)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Restart
+            // 
+            this.Restart.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Restart.Location = new System.Drawing.Point(423, 535);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(250, 50);
+            this.Restart.TabIndex = 1;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Highest Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(536, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Score";
+            // 
+            // numberselector
+            // 
+            this.numberselector.Location = new System.Drawing.Point(274, 551);
+            this.numberselector.Name = "numberselector";
+            this.numberselector.Size = new System.Drawing.Size(143, 29);
+            this.numberselector.TabIndex = 4;
+            this.numberselector.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NewGame
+            // 
+            this.NewGame.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NewGame.Location = new System.Drawing.Point(12, 535);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(256, 50);
+            this.NewGame.TabIndex = 7;
+            this.NewGame.Text = "NewGame";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // HSlabel
+            // 
+            this.HSlabel.AutoSize = true;
+            this.HSlabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.HSlabel.Location = new System.Drawing.Point(216, 13);
+            this.HSlabel.Name = "HSlabel";
+            this.HSlabel.Size = new System.Drawing.Size(42, 31);
+            this.HSlabel.TabIndex = 8;
+            this.HSlabel.Text = "00";
+            // 
+            // Slabel
+            // 
+            this.Slabel.AutoSize = true;
+            this.Slabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Slabel.Location = new System.Drawing.Point(631, 9);
+            this.Slabel.Name = "Slabel";
+            this.Slabel.Size = new System.Drawing.Size(42, 31);
+            this.Slabel.TabIndex = 9;
+            this.Slabel.Text = "00";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 399);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(661, 130);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Exit.Location = new System.Drawing.Point(264, 5);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(251, 35);
+            this.Exit.TabIndex = 11;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(18, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(655, 346);
+            this.panel1.TabIndex = 12;
+            // 
+            // Homework1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(685, 597);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Slabel);
+            this.Controls.Add(this.HSlabel);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.numberselector);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Restart);
+            this.Name = "Homework1";
+            this.Text = "Homework1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numberselector)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numberselector;
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Label HSlabel;
+        private System.Windows.Forms.Label Slabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CsharpHomework1/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpHomework1
+{
+    public partial class Homework1 : Form
+    {
+        //全域變數群
+        Button[] buttons = new Button[1000]; //動態按鈕
+        int[] ay = new int[1000]; //存亂數值，順便給Restart用
+        int[] ay2 = new int[1000]; //備存亂數值
+        int[] locationRow = new int[1000];
+        int[] locationColumn = new int[1000];
+        int ns; //存選擇器內容
+        int ns2;//備存選擇器
+        Random random = new Random(); //亂數Class
+        int i = 0; //給迴圈用
+        int score = 0;
+        int highestscore = 0;
+
+        public Homework1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Restart.Enabled = false; //Restart初始化為不能點
+            MessageBox.Show("歡迎光臨", "我做好久");
+            listBox1.Items.Add("Click Button(列,行)with the value of \"Value\"\r\n");
+            for (int k = 0; k <= 100; k++) //陣列初始化 沒有這個會錯!!
+            {
+                ay[k] = 100;
+                ay2[k] = 100;
+            }
+        }
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            //初始化
+            panel1.Controls.Clear();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Click Button(列,行)with the value of \"Value\"\r\n");
+            int count = 0;
+            int column = 0;
+            int row = 0;
+            //雜項
+            Slabel.Text = "00"; //Restart分數歸零
+            score = 0; //Restart分數歸零
+            //
+            ns2 = ns; //ns2已被減至0 所以調回來!
+            for (int k = 0; k <= 100; k++) //ay[]值調回儲存值
+            {
+                ay[k] = ay2[k];
+            }
+            //
+            for (i = 0; i < ns; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Text = ay[i] + ""; //存的亂數值派上用場
+                buttons[i].Name = "button" + i; //給每個按鈕名字
+                buttons[i].Location = new Point(12 + (column * 40), 12 + (row * 20));
+                buttons[i].Size = new Size(40, 20);
+                buttons[i].TabIndex = ay[i]; //存的亂數值派上用場
+                buttons[i].Click += new EventHandler(buttons_Click);
+                column += 1; //往右移動一格~!
+                count += 1; //計數器 有幾個按鈕
+                this.panel1.Controls.Add(buttons[i]);
+                if (count % 10 == 0) //一列有十個就
+                {
+                    column = 0; //換到第一行
+                    row += 1; //跑到下一列
+                }
+            }
+        }
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            //初始化
+            panel1.Controls.Clear();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Click Button(列,行)with the value of \"Value\"\r\n");
+            int count = 0;
+            int column = 0;
+            int row = 0;
+            //雜項
+            Restart.Enabled = true; //點NewGame,Restart才能點
+            HSlabel.Text = "00"; //最高分數初始化
+            highestscore = 0; //最高分數初始化
+            Slabel.Text = "00"; //分數歸零
+            score = 0; //分數歸零
+            //
+            ns = (int)numberselector.Value; //得到選擇器內容並強制轉型
+            ns2 = ns; //第一次存ns2
+            //
+            for (i = 0; i < ns; i++)
+            {
+                //跟上面比 多的行數
+                int onetoten = random.Next(1, 11); //亂數只有1~10
+                ay[i] = onetoten; //存亂數值
+                ay2[i] = onetoten;
+                locationRow[i] = row + 1; //存列行給歷史記錄用
+                locationColumn[i] = column + 1; //存列行給歷史記錄用
+                //
+                buttons[i] = new Button();
+                buttons[i].Text = onetoten + "";
+                buttons[i].Name = "button" + i; //給每個按鈕名字
+                buttons[i].Location = new Point(12 + (column * 40), 12 + (row * 20));
+                buttons[i].Size = new Size(40, 20);
+                buttons[i].TabIndex = onetoten; //索引值
+                buttons[i].Click += new EventHandler(buttons_Click);
+                column += 1; //往右移動一格~!
+                count += 1; //計數器 有幾個按鈕
+                this.panel1.Controls.Add(buttons[i]);
+                if (count % 10 == 0) //一列有十個就
+                {
+                    column = 0; //換到第一行 
+                    row += 1; //跑到下一列
+                }
+            }
+
+        }
+        private void buttons_Click(object sender, EventArgs e)
+        {
+            //歷史紀錄
+            Console.WriteLine(((Button)(sender)).Name);
+            Console.WriteLine(((Button)(sender)).Text);
+            for (int k = 0; k <= 100; k++)
+            {
+                if ((sender as Button).Name == "button" + k)
+                {
+                    listBox1.Items.Add("Click Button(" + locationRow[k] + "," + locationColumn[k] + ")with the value of " + ((Button)(sender)).Text + "\r\n");
+                }
+            }
+            //存按鈕的值
+            Console.WriteLine(((Button)(sender)).TabIndex);
+            int x = ((Button)(sender)).TabIndex;
+            //刪除按鈕自己
+            Console.WriteLine(((Button)(sender)).Name);
+            for (int k = 0; k <= 100; k++)
+            {
+                if ((sender as Button).Name == "button" + k)
+                {
+                    panel1.Controls.Remove(buttons[k]);
+                    ns2--; //減少直到0
+                }
+            }
+            //刪除值為k-1&k+1的按鈕
+            for (int k = 0; k <= 100; k++)
+            {
+                if (ay[k] == x + 1)
+                {
+                    panel1.Controls.Remove(buttons[k]);
+                    ns2--; //減少直到0
+                    ay[k] = 100; //別讓他出來搗亂
+                }
+            }
+            for (int k = 0; k <= 100; k++)
+            {
+                if (ay[k] == x - 1)
+                {
+                    panel1.Controls.Remove(buttons[k]);
+                    ns2--; //減少直到0
+                    ay[k] = 100; //別讓他出來搗亂
+                }
+            }
+            //分數判斷
+            score += x;
+            Slabel.Text = score + "";
+            if (score > highestscore)
+            {
+                highestscore = score;
+                HSlabel.Text = score + "";
+            }
+            //遊戲結束 得到結果
+            if (ns2 == 0)
+            {
+                MessageBox.Show(score + "/" + highestscore, "Result");
+            }
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //離開
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
